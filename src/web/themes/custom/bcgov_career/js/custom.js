1adcc6d5bbcc1b8cc5ff68cfe7d401bc8a179a2c
Gurjinder Singh <gurjinder_12@yahoo.com>
2022-03-31 08:40:36 -0700

    color line bug fix

--- src/web/themes/custom/bcgov_career/js/custom.js
+++ src/web/themes/custom/bcgov_career/js/custom.js
@@ -278,7 +278,7 @@
       $("#block-views-block-step-form-pagination-block-1")
         .prev("#block-bcgov-career-content")
         .addClass("no-border-block");
-      $(".path-quiz.quiz-color-dark-blue .block-forms-steps")
+      $(".path-quiz .block-forms-steps")
         .prev("#block-bcgov-career-content")
         .addClass("no-border-block");
     },
