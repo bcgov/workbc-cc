c34b2de2bf9bc7f11b4a5876e839afaaf1268d4d
Gurjinder Singh <gurjinder_12@yahoo.com>
2022-05-11 11:37:11 -0700

    slider work quiz changes

--- src/web/modules/custom/work_bc_quiz/js/work_bc_quiz.js
+++ src/web/modules/custom/work_bc_quiz/js/work_bc_quiz.js
@@ -1,14 +1,6 @@
 (function ($, Drupal) {
     Drupal.behaviors.workbc = { 
         attach: function (context, settings) {
-            $(".wvq .hideshow .vaa").click(function(){
-
-             $(".extradivs1").animate({marginLeft: "-100%"}, 1500);
-            });
-            $(".wvq .hideshow .vaa1").click(function(){
-             $(".extradivs1").animate({marginLeft: "-0"}, 1500);
-
-            });
             
         }
     }
