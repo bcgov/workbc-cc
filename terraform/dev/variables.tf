f40ca71d061fc16e1937ccfd116e3e6f7aa455d0
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-05-18 16:21:52 -0700

    Update variables.tf

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -13,10 +13,10 @@ variable "aws_region" {
   default     = "ca-central-1"
 }
 
-#variable "ecs_task_execution_role_name" {
-#  description = "ECS task execution role name"
-#  default     = "startupSampleEcsTaskExecutionRole"
-#}
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default     = "workbcCcEcsTaskExecutionRole"
+}
 
 #variable "ecs_auto_scale_role_name" {
 #  description = "ECS auto scale role Name"
