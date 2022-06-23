e6334cad645bfdf0e49bc1e20730886de39728ea
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-06-23 09:42:51 -0700

    Update ecs.tf

--- terraform/prod/ecs.tf
+++ terraform/prod/ecs.tf
@@ -100,7 +100,7 @@ resource "aws_ecs_task_definition" "app" {
 			},
 			{
 				name = "POSTGRES_HOST",
-				value = "${aws_rds_cluster_instance.postgres[1].endpoint}"
+				value = "${aws_rds_cluster.postgres.endpoint}"
 			}
 		]
 		secrets = [
@@ -200,7 +200,7 @@ resource "aws_ecs_task_definition" "app" {
 			},
 			{
 				name = "POSTGRES_HOST",
-				value = "${aws_rds_cluster_instance.postgres[1].endpoint}"
+				value = "${aws_rds_cluster.postgres.endpoint}"
 			}
 		]
 		secrets = [
