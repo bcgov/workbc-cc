3c7a288c1d48082c5698b865ed4ac2f102bd75a4
infojunkie <karim.ratib@gmail.com>
2023-10-16 17:38:48 -0700

    Local development environment

---
+++ src/web/sites/default/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'workbc-cc',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'postgres',
+  'port' => '5432',
+  'driver' => 'pgsql',
+);
