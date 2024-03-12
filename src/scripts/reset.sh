ee5651383bee27ebb6c0ea42497d5c7284d02ad9
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-03-12 13:15:08 -0700

    career profile migration & taxonomy level updates

---
+++ src/scripts/reset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Reset the local Drupal site with a dump from the Backup/Restore files.
+#
+if [ -z "$1" ]; then
+  php drush bamls --files=private_files
+  exit
+fi
+
+drush bamr default_db private_files $1
