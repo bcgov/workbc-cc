58124cbf1dc62a03e4371af533ab00ec1bd62443
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-06-17 12:03:44 -0700

    configuration

---
+++ src/web/sites/default/settings.local.php
@@ -0,0 +1,18 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'workbc-cc',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'postgres',
+  'port' => '5432',
+  'driver' => 'pgsql',
+);
+
+
+ini_set('memory_limit', '1G');
+if (class_exists('Kint')) {
+  // Set the max_depth to prevent out-of-memory.
+  \Kint::$depth_limit = 4;
+}
