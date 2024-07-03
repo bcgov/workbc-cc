d075f4951ae8ff46bc70e94a8309c0e1ec1dd90a
infojunkie <karim.ratib@gmail.com>
2024-07-03 16:03:34 -0700

    Reusable quiz scoring

--- src/web/modules/custom/workbc_cdq_custom/src/Controller/QuizResultsController.php
+++ src/web/modules/custom/workbc_cdq_custom/src/Controller/QuizResultsController.php
@@ -8,37 +8,43 @@
 namespace Drupal\workbc_cdq_custom\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\webform\Entity\Webform;
-use Drupal\webform\Entity\WebformSubmission;
-
 
 class QuizResultsController extends ControllerBase {
 
+  protected function getScores($submission) {
+    $quiz = $submission->getWebform();
+    $questions = $quiz->getElementsDecodedAndFlattened();
+    $answers = $submission->getData();
+    $settings = $quiz->get('third_party_settings');
+    $categories = array_reduce(\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($settings['workbc_cdq_career_match']['categories']), function($categories, $category) {
+      $categories[$category->name] = [
+        'score' => 0,
+        'count' => 0,
+        'term' => $category,
+      ];
+      return $categories;
+    });
+    $results = [];
+    foreach ($answers as $key => $value) {
+      $category = $questions[$key]['#category'];
+      $score = floatval($value);
+      $categories[$category]['score'] += $score;
+      $categories[$category]['count'] += 1;
+      $results[$questions[$key]['#category']] = $score;
+    }
+    return [
+      'categories' => $categories,
+      'answers' => $results,
+    ];
+  }
 
   public function abilities_quiz_results() {
-
-    $results = getUserSubmission('abilities_quiz');
-    if ($results) {
-      $quiz = $results->getWebform();
-      $questions = $quiz->getElementsDecodedAndFlattened();
-      $answers = $results->getData();
-      $settings = $quiz->get('third_party_settings');
-      $categories = array_reduce(\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($settings['workbc_cdq_career_match']['categories']), function($categories, $category) {
-        $categories[$category->name] = 0;
-        return $categories;
-      });
-
+    $submission = getUserSubmission('abilities_quiz');
+    if ($submission) {
       $markup = "";
-      foreach ($answers as $key => $value) {
-        $question = $questions[$key]['#title'];
-        $category = $questions[$key]['#category'];
-        $score = floatval($value);
-        $markup .= "<p>$question ($category): <b>$score</b></p>";
-        $categories[$category] += $score;
-      }
-      $markup .= "<hr>";
-      foreach ($categories as $category => $score) {
-        $markup .= "<p>$category: <b>$score</b></p>";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
       }
     }
     else {
@@ -55,21 +61,13 @@ class QuizResultsController extends ControllerBase {
 
   public function work_preferences_quiz_results() {
 
-    $results = getUserSubmission('work_preferences_quiz');
-    if ($results) {
-
-      $data = $results->getData();
-
+    $submission = getUserSubmission('work_preferences_quiz');
+    if ($submission) {
       $markup = "";
-
-      // $total = 0;
-      // foreach ($data as $key => $value) {
-      //   $markup .= "<p>" . $key . ": <b>" . $value . "</b></p>";
-      //   $total += $value;
-      // }
-
-      // $markup .= "<p></p>";
-      // $markup .= "<p>Total Score: <b>" . $total . "</b></p>";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
+      }
     }
     else {
       $markup = "No results found";
@@ -85,21 +83,13 @@ class QuizResultsController extends ControllerBase {
 
   public function interests_quiz_results() {
 
-    $results = getUserSubmission('interests_quiz');
-    if ($results) {
-
-      $data = $results->getData();
-
+    $submission = getUserSubmission('interests_quiz');
+    if ($submission) {
       $markup = "";
-
-      // $total = 0;
-      // foreach ($data as $key => $value) {
-      //   $markup .= "<p>" . $key . ": <b>" . $value . "</b></p>";
-      //   $total += $value;
-      // }
-
-      // $markup .= "<p></p>";
-      // $markup .= "<p>Total Score: <b>" . $total . "</b></p>";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
+      }
     }
     else {
       $markup = "No results found";
@@ -112,47 +102,63 @@ class QuizResultsController extends ControllerBase {
 
   }
 
+  public function multiple_intelligences_quiz_results() {
 
-  public function import_career_profile_updates() {
-
-    $profiles = $this->loadCareerProfileUpdates();
-
-    foreach($profiles as $profile) {
-      $nodes = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->loadByProperties(['field_noc' => $profile[0]]);
-      $node = reset($nodes);
-      if ($node) {
-        $node->field_video_id = $profile[2];
-        $node->field_noc_2016 = $profile[1];
-        $node->save();
+    $submission = getUserSubmission('multiple_intelligences_quiz');
+    if ($submission) {
+      $markup = "";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
       }
     }
-
-    $markup = "Career Profile video id import";
-
+    else {
+      $markup = "No results found";
+    }
     return [
       '#type' => 'markup',
-      '#markup' => $markup,
+      '#markup' => '',
       '#cache' => ['max-age' => 0],
     ];
   }
 
+  public function learning_styles_quiz_results() {
 
-  function loadCareerProfileUpdates() {
-    $path = DRUPAL_ROOT;
-    $path = preg_replace('/web$/', '', $path);
-    $file = fopen($path . "cdq-career-profile-update.csv", 'r');
-    if($file !== FALSE){
-      $videosIds = [];
-      while (($item = fgetcsv($file, 0, "|")) !== FALSE) {
-        if (!empty($item[1])) {
-          $videosIds[] = $item;
-        }
+    $submission = getUserSubmission('learning_styles_quiz');
+    if ($submission) {
+      $markup = "";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
       }
-      fclose($file);
-
     }
-    return empty($videosIds) ? false : $videosIds;
+    else {
+      $markup = "No results found";
+    }
+    return [
+      '#type' => 'markup',
+      '#markup' => '',
+      '#cache' => ['max-age' => 0],
+    ];
+  }
+
+  public function work_values_quiz_results() {
+
+    $submission = getUserSubmission('work_values_quiz');
+    if ($submission) {
+      $markup = "";
+      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+        $percent = $data['score'] / ($data['count'] * 4) * 100;
+        $markup .= "<p>$category: <b>$percent%</b></p>";
+      }
+    }
+    else {
+      $markup = "No results found";
+    }
+    return [
+      '#type' => 'markup',
+      '#markup' => '',
+      '#cache' => ['max-age' => 0],
+    ];
   }
 }
