a0a356d142a73033ac61cd5c109926fac2cf7bc6
infojunkie <karim.ratib@gmail.com>
2024-07-04 15:50:07 -0700

    Career matching for Abilities quiz

--- src/web/modules/custom/workbc_cdq_custom/src/Controller/QuizResultsController.php
+++ src/web/modules/custom/workbc_cdq_custom/src/Controller/QuizResultsController.php
@@ -9,58 +9,23 @@ namespace Drupal\workbc_cdq_custom\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-function round_up($value, $precision=0) {
-  $power = pow(10,$precision);
-  return ceil($value*$power)/$power;
-}
-
-function round_down($value, $precision=0) {
-  $power = pow(10,$precision);
-  return floor($value*$power)/$power;
-}
-
 class QuizResultsController extends ControllerBase {
-
-  protected function getScores($submission) {
-    $quiz = $submission->getWebform();
-    $questions = $quiz->getElementsDecodedAndFlattened();
-    $answers = $submission->getData();
-    $settings = $quiz->get('third_party_settings');
-    $categories = array_reduce(\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($settings['workbc_cdq_career_match']['categories']), function($categories, $category) {
-      $categories[$category->name] = [
-        'score' => 0,
-        'count' => 0,
-        'term' => $category,
-      ];
-      return $categories;
-    });
-    $results = [];
-    foreach ($answers as $key => $value) {
-      $category = $questions[$key]['#category'];
-      $score = floatval($value);
-      $categories[$category]['score'] += $score;
-      $categories[$category]['count'] += 1;
-      $results[$questions[$key]['#category']] = $score;
-    }
-    foreach ($categories as &$category) {
-      $category['percent'] = $category['score'] / ($category['count'] * 4);
-      $category['normalized'] = round_down($category['score'] / $category['count'], 1) + 1;
-    }
-    return [
-      'categories' => $categories,
-      'answers' => $results,
-    ];
-  }
 
   public function abilities_quiz_results() {
     $submission = getUserSubmission('abilities_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category_name => $category) {
+      $score = getSubmissionScore($submission);
+      foreach ($score['categories'] as $category_name => $category) {
         $percent = $category['percent'] * 100;
         $normalized = $category['normalized'];
         $markup .= "<p>$category_name: <b>$percent% ($normalized)</b></p>";
       }
+      $markup .= '<hr>';
+      $matches = matchAbilitiesCareers($submission, $score);
+      foreach ($matches as $match) {
+        $markup .= "<p>{$match['noc']}: <b>{$match['match']}%</b></p>";
+      }
     }
     else {
       $markup = "No results found";
@@ -70,16 +35,13 @@ class QuizResultsController extends ControllerBase {
       '#markup' => $markup,
       '#cache' => ['max-age' => 0],
     ];
-
   }
 
-
   public function work_preferences_quiz_results() {
-
     $submission = getUserSubmission('work_preferences_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+      foreach (getSubmissionScore($submission)['categories'] as $category => $data) {
         $percent = $data['score'] / ($data['count'] * 4) * 100;
         $markup .= "<p>$category: <b>$percent%</b></p>";
       }
@@ -92,16 +54,13 @@ class QuizResultsController extends ControllerBase {
       '#markup' => $markup,
       '#cache' => ['max-age' => 0],
     ];
-
   }
 
-
   public function interests_quiz_results() {
-
     $submission = getUserSubmission('interests_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+      foreach (getSubmissionScore($submission)['categories'] as $category => $data) {
         $percent = $data['score'] / ($data['count'] * 4) * 100;
         $markup .= "<p>$category: <b>$percent%</b></p>";
       }
@@ -114,15 +73,13 @@ class QuizResultsController extends ControllerBase {
       '#markup' => $markup,
       '#cache' => ['max-age' => 0],
     ];
-
   }
 
   public function multiple_intelligences_quiz_results() {
-
     $submission = getUserSubmission('multiple_intelligences_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+      foreach (getSubmissionScore($submission)['categories'] as $category => $data) {
         $percent = $data['score'] / ($data['count'] * 4) * 100;
         $markup .= "<p>$category: <b>$percent%</b></p>";
       }
@@ -138,11 +95,10 @@ class QuizResultsController extends ControllerBase {
   }
 
   public function learning_styles_quiz_results() {
-
     $submission = getUserSubmission('learning_styles_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+      foreach (getSubmissionScore($submission)['categories'] as $category => $data) {
         $percent = $data['score'] / ($data['count'] * 4) * 100;
         $markup .= "<p>$category: <b>$percent%</b></p>";
       }
@@ -158,11 +114,10 @@ class QuizResultsController extends ControllerBase {
   }
 
   public function work_values_quiz_results() {
-
     $submission = getUserSubmission('work_values_quiz');
     if ($submission) {
       $markup = "";
-      foreach ($this->getScores($submission)['categories'] as $category => $data) {
+      foreach (getSubmissionScore($submission)['categories'] as $category => $data) {
         $percent = $data['score'] / ($data['count'] * 4) * 100;
         $markup .= "<p>$category: <b>$percent%</b></p>";
       }
