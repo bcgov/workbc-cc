9446579f0cf4ca08cabf9ab306aece8567866d67
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-07-24 16:28:12 -0700

    wip

---
+++ src/web/modules/custom/workbc_cdq_custom/src/Plugin/Block/QuizPageTitleBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\workbc_cdq_custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
+
+
+/**
+ * Provides a WorkBC Career Discover Quizzes block.
+ *
+ * @Block(
+ *   id = "workbc_quiz_page_title",
+ *   admin_label = @Translation("WorkBC Quiz Page Title"),
+ *   category = @Translation("WorkBC - CDQ"),
+ * )
+ */
+class QuizPageTitleBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $id = "abilities_quiz";
+    $quiz = workbc_cdq_quiz_info($id);
+// ksm($quiz);
+    $markup = "";
+    $markup .= '<h2 class="cdq-quiz-title">' . $quiz['title'] . '</h2>';
+    $markup .= '<div class="sub-title">';
+    $markup .= $quiz['subtitle'];
+    $markup .= '</div>';
+    $markup .= '<div class="node-questions">';
+    $markup .= '<div class="quiz-questions"> <span></span>' . $quiz['count'] . ' questions</div>';
+    $markup .= '<div class="quiz-duration"> <span></span>' . $quiz['time'] .'</div>';
+    $markup .= '</div>';
+
+
+    return array(
+      '#type' => 'markup',
+      '#markup' => $markup,
+    );
+
+  }
+
+  public function getCacheMaxAge() {
+      return 0;
+  }
+
+}
