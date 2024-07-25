50861c0198cc55de4f716a224d83c144f6681813
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-07-25 15:54:07 -0700

    Quiz page theming

--- src/web/modules/custom/workbc_cdq_custom/src/Plugin/Block/QuizPageTitleBlock.php
+++ src/web/modules/custom/workbc_cdq_custom/src/Plugin/Block/QuizPageTitleBlock.php
@@ -25,38 +25,25 @@ class QuizPageTitleBlock extends BlockBase {
    */
   public function build() {
 
-    $colors = [
-      'abilities_quiz' => 'cdq-quiz-dark-blue',
-      'work_preferences_quiz' => 'cdq-quiz-purple',
-      'interests_quiz' => 'cdq-quiz-blue',
-      'multiple_intelligencies_quiz' => 'cdq-quiz-green',
-      'learning_styles_quiz' => 'cdq-quiz-light-blue',
-      'work_values_quiz' => 'cdq-quiz-pink'
-    ];
-
-    $path = \Drupal::service('path.current')->getPath();
-    $path = explode("/", $path);
-
-    $quiz = workbc_cdq_quiz_info($path[2]);
-
-
     $markup = "";
-    $markup .= '<h2 class="cdq-quiz-title">' . $quiz['title'] . '</h2>';
-    $markup .= '<div class="sub-title">';
-    $markup .= $quiz['subtitle'];
-    $markup .= '</div>';
-    $markup .= '<div class="node-questions">';
-    $markup .= '<div class="quiz-questions"> <span></span>' . $quiz['count'] . ' questions</div>';
-    $markup .= '<div class="quiz-duration"> <span></span>' . $quiz['time'] .'</div>';
-    $markup .= '</div>';
+    $class = "";
 
+    $current_quiz = getCurrentQuiz();
+    if ($current_quiz) {
+      $quiz = workbc_cdq_quiz_info($current_quiz);
+      $markup .= '<h2 class="cdq-quiz-title">' . $quiz['title'] . '</h2>';
+      $markup .= '<div class="sub-title">';
+      $markup .= $quiz['subtitle'];
+      $markup .= '</div>';
+      $markup .= '<div class="node-questions">';
+      $markup .= '<div class="quiz-questions"> <span></span>' . $quiz['count'] . ' questions</div>';
+      $markup .= '<div class="quiz-duration"> <span></span>' . $quiz['time'] .'</div>';
+      $markup .= '</div>';
+    }
 
     return array(
       '#type' => 'markup',
       '#markup' => $markup,
-      '#attributes' => [
-        'class' => [$colors[$path[2]]]
-      ]
     );
 
   }
