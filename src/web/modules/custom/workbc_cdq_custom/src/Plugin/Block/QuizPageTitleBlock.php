217eb5a8213b74174663a324d869bac2ccc51555
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-07-25 09:02:31 -0700

    wip

--- src/web/modules/custom/workbc_cdq_custom/src/Plugin/Block/QuizPageTitleBlock.php
+++ src/web/modules/custom/workbc_cdq_custom/src/Plugin/Block/QuizPageTitleBlock.php
@@ -25,10 +25,20 @@ class QuizPageTitleBlock extends BlockBase {
    */
   public function build() {
 
+    $colors = [
+      'abilities_quiz' => 'cdq-quiz-dark-blue',
+      'work_preferences_quiz' => 'cdq-quiz-purple',
+      'interests_quiz' => 'cdq-quiz-blue',
+      'multiple_intelligencies_quiz' => 'cdq-quiz-green',
+      'learning_styles_quiz' => 'cdq-quiz-light-blue',
+      'work_values_quiz' => 'cdq-quiz-pink'
+    ];
+
     $path = \Drupal::service('path.current')->getPath();
     $path = explode("/", $path);
 
     $quiz = workbc_cdq_quiz_info($path[2]);
+
 
     $markup = "";
     $markup .= '<h2 class="cdq-quiz-title">' . $quiz['title'] . '</h2>';
@@ -44,6 +54,9 @@ class QuizPageTitleBlock extends BlockBase {
     return array(
       '#type' => 'markup',
       '#markup' => $markup,
+      '#attributes' => [
+        'class' => [$colors[$path[2]]]
+      ]
     );
 
   }
