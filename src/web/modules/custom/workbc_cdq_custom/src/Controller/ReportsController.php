5eccd36edcd85ffe369657617ec1a3e4c9f5ac08
infojunkie <karim.ratib@gmail.com>
2024-07-30 12:02:12 -0700

    [WBCAMS-507] WorkBC environment report

---
+++ src/web/modules/custom/workbc_cdq_custom/src/Controller/ReportsController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * CustomModalController class.
+ */
+
+namespace Drupal\workbc_cdq_custom\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ReportsController extends ControllerBase {
+  public function environment() {
+    ob_start();
+    phpinfo((INFO_VARIABLES | INFO_ENVIRONMENT));
+    $env = ob_get_clean();
+    return [
+      '#type' => 'markup',
+      '#markup' => $env,
+    ];
+  }
+}
