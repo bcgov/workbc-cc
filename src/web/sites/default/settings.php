62e830291f9e101a2d71292478199eeb8c53c608
rjkuyvenhoven <rjkuyvenhoven@gmail.com>
2024-08-01 14:40:45 -0700

    disable trusted_host_patterns for testing purposes

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -836,21 +836,21 @@ $settings['file_private_path'] = '../private';
 $settings['file_tmp_path'] = '/tmp';
 
 // Trusted host entries.
-$settings['trusted_host_patterns'] = [
-  '^localhost$',
-  '^127\.0\.0\.1$',
-  'workbc-cc\.docker\.localhost',
-  'workbc-cc\.localhost',
-  '^careercompass-dev\.workbc\.ca$',
-  '^careerdiscoveryquizzes-dev\.workbc\.ca$',
-  '^careerdiscoveryquizzes-test\.workbc\.ca$',
-  '^careerdiscoveryquizzes\.workbc\.ca$',
-  '^career\.lndo\.site$',
-  '^career\.ddev\.site$',
-  '^workbc-cc\.b89n0c-prod\.nimbus\.cloud\.gov\.bc\.ca$',
-  '^workbc-cc\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
-  '^workbc-cc\.b89n0c-test\.nimbus\.cloud\.gov\.bc\.ca$',
-];
+// $settings['trusted_host_patterns'] = [
+//   '^localhost$',
+//   '^127\.0\.0\.1$',
+//   'workbc-cc\.docker\.localhost',
+//   'workbc-cc\.localhost',
+//   '^careercompass-dev\.workbc\.ca$',
+//   '^careerdiscoveryquizzes-dev\.workbc\.ca$',
+//   '^careerdiscoveryquizzes-test\.workbc\.ca$',
+//   '^careerdiscoveryquizzes\.workbc\.ca$',
+//   '^career\.lndo\.site$',
+//   '^career\.ddev\.site$',
+//   '^workbc-cc\.b89n0c-prod\.nimbus\.cloud\.gov\.bc\.ca$',
+//   '^workbc-cc\.b89n0c-dev\.nimbus\.cloud\.gov\.bc\.ca$',
+//   '^workbc-cc\.b89n0c-test\.nimbus\.cloud\.gov\.bc\.ca$',
+// ];
 
 $config['workbc']['onet'] = [
   'username' => getenv('ONET_USERNAME'),
