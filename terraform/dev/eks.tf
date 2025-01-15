52312016f301a81e37a2d293a14843cf5253054c
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-01-15 13:44:12 -0800

    Update eks.tf

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -1,3 +1,28 @@
+#Cluster role
+resource "aws_iam_role" "eks-cluster-role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster-role.name
+}
+
 resource "aws_eks_cluster" "workbc-cluster" {
   name = "workbc-cluster"
   access_config {
