e646aef94efbadcbc29ee55dfb04dd503337a877
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-01-16 09:24:11 -0800

    Update eks.tf

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -39,3 +39,61 @@ resource "aws_eks_cluster" "workbc-cluster" {
     aws_iam_role.eks-cluster-role,
   ]
 }
+
+#Node group role
+resource "aws_iam_role" "eks-ng-role" {
+  name = "eks-ng-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+#Node group policies
+resource "aws_iam_role_policy_attachment" "ng-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-ng-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ng-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-ng-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ng-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-ng-role.name
+}
+
+#Node group
+resource "aws_eks_node_group" "eks-ng" {
+  cluster_name    = aws_eks_cluster.workbc-cluster.name
+  node_group_name = "eks-ng"
+  node_role_arn   = aws_iam_role.eks-ng-role.arn
+  subnet_ids      = module.network.aws_subnet_ids.app.ids
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.ng-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.ng-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.ng-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
